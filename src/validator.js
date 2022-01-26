686a5cf319951741a0ba95c8320fa4b0385cb4fe
Lizeth Rodriguez <lizeth_775@hotmail.com>
2022-01-25 21:33:07 -0500

    Validador tarjeta y ocultar caracteres

--- src/validator.js
+++ src/validator.js
@@ -1,27 +1,22 @@
-/* Convertimos isValid en funcion*/
+/* eslint-disable no-console */
 const validator = {
   isValid: function(creditCardNumber){
     /* delcaramos las variables a usar mas adelante*/
     let multiplicacionPares = []
     let doble = null
-/* por que null*/    
     let esValido = false
     let sumaDigitos = 0
-/* hace falta (,) ó (;)? al declarar las variables*/     
-    
-    /* Declaramos una variable para los arrays en orden invertido*/
     let nroTarjetaInvertido = []
-    
+
     /* Declaramos una variables para los datos ingresados y los convertimos en array con split */
-    let arrayNroTarjeta = creditCardNumber.split("")  
+    let arrayNroTarjeta = creditCardNumber.split("")
 
     /* Iniciamos un for donde hacemos que los datos convertidos en arrays se ordenen en forma inversa, indice de datos inicia en cero por lo que se resta -1 iterar desde i hasta 0, en cada push enviamos el i a la variable */
-
     for(let i = arrayNroTarjeta.length -1; i >= 0; i-- ){
       nroTarjetaInvertido.push(arrayNroTarjeta[i])
     }
 
-    /*Otra alternativa en una sola linea para retornar los datos invertidos */
+    //Otra alternativa en una sola linea para retornar los datos invertidos
     //console.log(nroTarjeta.split("").reverse())
 
     for(let i = 0; i < nroTarjetaInvertido.length; i++){
@@ -38,7 +33,7 @@ const validator = {
         multiplicacionPares.push(+nroTarjetaInvertido[i])
       }
     }
-    
+
 
     for(let i = 0;  i < multiplicacionPares.length; i++) {
       sumaDigitos += multiplicacionPares[i]
@@ -46,25 +41,26 @@ const validator = {
 
     if(sumaDigitos%10 === 0) esValido = true
 
- /*   if(esvalido===true)*/
-    
-    alert(esValido)
+
+
+    console.log(`IsValid: ${esValido}`)
 
     return esValido
 
   },
-  
+
   maskify: function(creditCardNumber){
-  
-    if (creditCardNumber.length <10) return creditCardNumber;
-	const last4Characters = creditCardNumber.substr(-4);
-	const maskingCharacters = creditCardNumber.substr(0, creditCardNumber.length - 5).replace(/\d/g, '#');
-	return `${maskingCharacters}${last4Characters}`;
-  
-  console.log(`${maskingCharacters}${last4Characters}`);
-  },
+   
+    if (creditCardNumber.length < 4) return creditCardNumber;
+
+    const ulitimos4Caracteres = creditCardNumber.substr(-4)
+    const convirtiendoANumeral = creditCardNumber.substr(0, creditCardNumber.length - 4).replace(/.(?=)/g, "#")
+
+    const numeroEnmascarado= `${convirtiendoANumeral}${ulitimos4Caracteres}`
+
+    return numeroEnmascarado
+
+    }
   };
 
-
   export default validator;
-  
